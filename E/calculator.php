71d13cf53904ad709c831c49c598abb5d5aa3342
rickosong <ricko.aprilianto00@gmail.com>
2021-07-17 10:05:00 +0800

    membuat calculator sederhana

---
+++ E/calculator.php
@@ -0,0 +1,68 @@
+<html>
+ <head>
+ <title>calculator php</title>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+ </head>
+ <body>
+ 
+ <div class="container" style="margin-top: 50px">
+ 
+ <?php
+ 
+ // jika tombol submit sudah ditekan
+ if(isset($_POST['submit']))
+ {
+ // Check number values
+ if(is_numeric($_POST['nomor1']) && is_numeric($_POST['nomor2']))
+ {
+ // Calculate total
+ if($_POST['operasi'] == 'tambah')
+ {
+ $total = $_POST['nomor1'] + $_POST['nomorr2']; 
+ }
+ if($_POST['operasi'] == 'kurang')
+ {
+ $total = $_POST['nomor1'] - $_POST['nomor2']; 
+ }
+ if($_POST['operasi'] == 'kali')
+ {
+ $total = $_POST['nomor1'] * $_POST['nomor2']; 
+ }
+ if($_POST['operasi'] == 'bagi')
+ {
+ $total = $_POST['nomor1'] / $_POST['nomor2']; 
+ }
+ 
+ // Print total to the browser
+ echo "<h1>{$_POST['nomor1']} {$_POST['operasi']} {$_POST['nomor2']} sama dengan {$total}</h1>";
+ 
+ } else {
+ 
+ // Print error message to the browser
+ echo 'Numeric values are required';
+ 
+ }
+ }
+ // end PHP. Code by webdevtrick.com
+ ?>
+     
+     <!-- Calculator form by webdevtrick.com -->
+     <form method="post" action="calculator.php">
+         <input name="nomor1" type="text" class="form-control" style="width: 150px; display: inline" />
+         <select name="operasi">
+          <option value="tambah">tambah</option>
+             <option value="kurang">kurang</option>
+             <option value="kali">kali</option>
+             <option value="bagi">bagi</option>
+         </select>
+         <input name="nomor2" type="text" class="form-control" style="width: 150px; display: inline" />
+         <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
+     </form>
+     
+ </div>
+ 
+ </body>
+</html>
